ea7615088a8c10a5cd5b33621690636f4e18deb7
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-05-30 18:52:25 -0500

    Subida de proyecto 2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function changeColor(event) {
     document.getElementById("main").style.backgroundColor = event.value;
 }
+function changeColor(x) {
+    document.getElementById("main").style.backgroundColor = x.value;
+}
